bdfd08ebb4c79645ca4c30ac917947e2860edc7b
kimghw <kimghw@krs.co.kr>
2025-07-23 10:06:16 +0900

    Clean up obsolete query assistant files
    - Remove templates directory (now using SQLite database)
    - Remove migrate_jsonl.py (migration completed)
    - Remove __pycache__ directories

--- modules/query_assistant/templates/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Query Templates Module - Now uses JSONL files in data/ folder"""
-
-# This module no longer contains hardcoded templates
-# Templates are now stored as JSONL files in the data/ directory
-# Use loader.py to load templates into VectorDB
